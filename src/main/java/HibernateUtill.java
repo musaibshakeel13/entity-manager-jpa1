6396e54c550b2055517b825705a54708ebd5c166
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-19 09:42:18 +0400

    Update HibernateUtill.java

--- src/main/java/HibernateUtill.java
+++ src/main/java/HibernateUtill.java
@@ -1,1 +1,28 @@
-
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//
+//public class HibernateUtill {
+////     creates session session factories etc this class is used for
+//    private  static SessionFactory sessionFactory;
+//
+//    static {
+//        try{
+//            sessionFactory = new Configuration()
+//                    .configure("hibernate.cfg.xml")
+//                    .addAnnotatedClass(UserClassHibernate.class)
+//                    .buildSessionFactory();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static Session getSession() {
+//        return sessionFactory.getCurrentSession();
+//    }
+//
+//    public static void close() {
+//        sessionFactory.close();
+//    }
+//}
